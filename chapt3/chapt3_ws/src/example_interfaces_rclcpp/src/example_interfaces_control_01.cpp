d36af440db542e7445cfe567800c1d7ed9a57874
MingzheYe <yemingzhe3@gmail.com>
2025-01-26 23:23:24 +0800

    finish ros2_interface

--- chapt3/chapt3_ws/src/example_interfaces_rclcpp/src/example_interfaces_control_01.cpp
+++ chapt3/chapt3_ws/src/example_interfaces_rclcpp/src/example_interfaces_control_01.cpp
@@ -6,14 +6,39 @@ class ExampleInterfacesControl : public rclcpp::Node {
 public:
     ExampleInterfacesControl(std::string name) : Node(name) {
         RCLCPP_INFO(this->get_logger(), "节点已启动：%s", name.c_str());
+        client_ = this->create_client<example_ros2_interfaces::srv::MoveRobot>("move_robot");   
+        robot_status_subscribe_ = this->create_subscription<example_ros2_interfaces::msg::RobotStatus>("robot_status",10,std::bind(&ExampleInterfacesControl::robot_status_callback, this, std::placeholders::_1));
+    }
+    void move_robot(float distance) {
+        RCLCPP_INFO(this->get_logger(), "请求移动机器人：%f", distance);
+        while(!client_->wait_for_service(std::chrono::seconds(1))) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(this->get_logger(), "等待服务的过程中被中断...");
+                return;
+            }
+            RCLCPP_INFO(this->get_logger(), "等待服务端上线中...");
+        }
+        auto request = std::make_shared<example_ros2_interfaces::srv::MoveRobot::Request>();
+        request->distance = distance;
+        
+        client_->async_send_request(request, std::bind(&ExampleInterfacesControl::result_callback_, this, std::placeholders::_1));
     }
 private:
-
+    rclcpp::Client<example_ros2_interfaces::srv::MoveRobot>::SharedPtr client_;
+    rclcpp::Subscription<example_ros2_interfaces::msg::RobotStatus>::SharedPtr robot_status_subscribe_;
+    void result_callback_(rclcpp::Client<example_ros2_interfaces::srv::MoveRobot>::SharedFuture result_future) {
+        auto response = result_future.get();
+        RCLCPP_INFO(this->get_logger(), "收到移动结果%f", response->pose);
+    }
+    void robot_status_callback(const example_ros2_interfaces::msg::RobotStatus::SharedPtr message) {
+        RCLCPP_INFO(this->get_logger(), "收到机器人状态：status=%d, pose=%f", message->status, message->pose);
+    }
 };
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<ExampleInterfacesControl>("example_interfaces_control_01");
+    node->move_robot(5.0);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
--- chapt3/chapt3_ws/src/example_interfaces_rclcpp/src/example_interfaces_robot_01.cpp
+++ chapt3/chapt3_ws/src/example_interfaces_rclcpp/src/example_interfaces_robot_01.cpp
@@ -1,8 +1,8 @@
 #include <cstdio>
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/srv/move_robot.hpp"
-#include "example_interfaces/msg/robot_status.hpp"
+#include "example_ros2_interfaces/srv/move_robot.hpp"
+#include "example_ros2_interfaces/msg/robot_status.hpp"
 
 /*
  * 测试指令：ros2 service call /move_robot example_ros2_interfaces/srv/MoveRobot "{distance: 5}"
@@ -18,11 +18,11 @@ public:
    * @param distance
    * @return float
    */
-   float move_distance(float distrance) {
+   float move_distance(float distance) {
     status_ = example_ros2_interfaces::msg::RobotStatus::STATUS_MOVING;
-    target_pose_ += distrance;
+    target_pose_ += distance;
     while (std::fabs(target_pose_ - current_pose_) > 0.01) {
-      float step = distrance/ std::fabs(distances) * std::fabs(target_pose_ - current_pose_) * 0.1;
+      float step = distance/ std::fabs(distance) * std::fabs(target_pose_ - current_pose_) * 0.1;
       current_pose_ += step;
       std::cout << "移动了：" << step << "，当前位置：" << current_pose_ << std::endl;
       std::this_thread::sleep_for(std::chrono::milliseconds(500));
@@ -42,8 +42,8 @@ class ExampleInterfacesRobot : public rclcpp::Node {
 public:
   ExampleInterfacesRobot(std::string name) : Node(name) {
     RCLCPP_INFO(this->get_logger(), "节点已启动：%s", name.c_str());
-    robot_status_server_ = this->create_service<example_ros2_interfaces::srv::MoveRobot>(
-      "move_robot", std::bind(&ExampleInterfacesRobot::move_robot_callback, this, std::placeholders::_1, std::placeholders::_2));
+    move_robot_server_ = this->create_service<example_ros2_interfaces::srv::MoveRobot>(
+      "move_robot", std::bind(&ExampleInterfacesRobot::handle_move_robot, this, std::placeholders::_1, std::placeholders::_2));
     robot_status_publisher_ = this->create_publisher<example_ros2_interfaces::msg::RobotStatus>("robot_status", 10);
     timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&ExampleInterfacesRobot::timer_callback, this));
   }
@@ -51,7 +51,7 @@ private:
   Robot robot;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Service<example_ros2_interfaces::srv::MoveRobot>::SharedPtr move_robot_server_;
-  rclcpp::Service<example_ros2_interfaces::msg::RobotStatus>::SharedPtr robot_status_publisher_;
+  rclcpp::Publisher<example_ros2_interfaces::msg::RobotStatus>::SharedPtr robot_status_publisher_;
 
   void timer_callback() {
     example_ros2_interfaces::msg::RobotStatus message;
